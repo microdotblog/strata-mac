4af8232d7fa5f5f5d84eeefadcc581e76b3990a7
Manton Reece <manton@me.com>
2024-11-04 16:50:44 -0600

    Added fetching notebooks and popup menu to change current notebook.

--- Strata/MBMainView.swift
+++ Strata/MBMainView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct MBMainView: View {
 	@State private var notes: [FeedItem] = []
+	@State private var notebooks: [FeedItem] = []
 	@State private var searchText = ""
 	@FocusState private var isSearchFocused: Bool
 	@State private var columnVisibility: NavigationSplitViewVisibility = .all
+	@State private var selectedNotebook: FeedItem?
 
 	var currentNotes: [FeedItem] {
 		if searchText.isEmpty {
@@ -52,13 +54,29 @@ struct MBMainView: View {
 		detail: {
 		}
 		.toolbar {
-			ToolbarItem(placement: .principal) {
+			ToolbarItem(placement: .navigation) {
+				Menu {
+					ForEach(notebooks, id: \.id) { notebook in
+						Button(notebook.title) {
+							self.selectedNotebook = notebook
+							self.fetchNotes()
+						}
+					}
+				} label: {
+					if let notebook = selectedNotebook {
+						Text(notebook.title)
+					}
+				}
+			}
+
+			ToolbarItem(placement: .automatic) {
 				TextField("Search", text: $searchText)
 					.focused($isSearchFocused)
 					.textFieldStyle(RoundedBorderTextFieldStyle())
 					.frame(width: 200)
 			}
-			ToolbarItem(placement: .primaryAction) {
+			
+			ToolbarItem(placement: .automatic) {
 				Button(action: {
 					newNote()
 				}) {
@@ -66,17 +84,18 @@ struct MBMainView: View {
 				}
 			}
 		}
+		.navigationTitle("")
 		.onAppear {
-			fetchNotes()
+			self.fetcNotebooks()
 		}
 		.onReceive(NotificationCenter.default.publisher(for: .focusSearchField)) { _ in
 			isSearchFocused = true
 		}
 	}
 
-	private func fetchNotes() {
+	private func fetcNotebooks() {
 		if let token = MBKeychain.shared.get(key: "Strata: Token") {
-			guard let url = URL(string: "https://micro.blog/notes/notebooks/1") else { return }
+			guard let url = URL(string: "https://micro.blog/notes/notebooks") else { return }
 			var request = URLRequest(url: url)
 			request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
 			
@@ -85,7 +104,9 @@ struct MBMainView: View {
 					do {
 						let feed = try JSONDecoder().decode(JSONFeed.self, from: data)
 						DispatchQueue.main.async {
-							self.notes = feed.items
+							self.notebooks = feed.items
+							self.selectedNotebook = feed.items.first
+							self.fetchNotes()
 						}
 					}
 					catch {
@@ -93,6 +114,31 @@ struct MBMainView: View {
 					}
 				}
 			}.resume()
+		}
+	}
+	
+	private func fetchNotes() {
+		if let notebook = self.selectedNotebook {
+			let notebook_id = notebook.id
+			if let token = MBKeychain.shared.get(key: "Strata: Token") {
+				guard let url = URL(string: "https://micro.blog/notes/notebooks/\(notebook_id)") else { return }
+				var request = URLRequest(url: url)
+				request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+				
+				URLSession.shared.dataTask(with: request) { data, response, error in
+					if let data = data {
+						do {
+							let feed = try JSONDecoder().decode(JSONFeed.self, from: data)
+							DispatchQueue.main.async {
+								self.notes = feed.items
+							}
+						}
+						catch {
+							print("Failed to decode JSON: \(error)")
+						}
+					}
+				}.resume()
+			}
 		}
 	}
 	
