bd14f8b909ecd30204027e0ed364cfbe165c658a
Manton Reece <manton@me.com>
2024-11-03 20:10:30 -0600

    Improved settings window and key field styling.

--- Strata/Source/MBSettingsView.swift
+++ Strata/Source/MBSettingsView.swift
@@ -9,26 +9,51 @@ import SwiftUI
 
 struct MBSettingsView: View {
 	@State private var secretKey: String = ""
+	@State private var oldSecretKey: String = ""
 
 	var body: some View {
 		VStack {
 			Text("Notes in Micro.blog are encrypted. To sync notes across devices, you will need to save a secret key so the notes can be decrypted later. If you lose your key, you will lose access to your notes too.")
 			
-			TextField("Secret Key", text: $secretKey)
-				.textFieldStyle(RoundedBorderTextFieldStyle())
-				.padding()
-
-			Button("Save") {
-				print("Secret: \(secretKey)")
-				if !MBKeychain.shared.save(key: "Strata: Secret", value: secretKey) {
-					print("Error saving secret key")
+			ZStack {
+				Rectangle()
+					.fill(Color("SettingsFieldBackground"))
+					.frame(height: 70)
+					.cornerRadius(5)
+					.overlay(
+						RoundedRectangle(cornerRadius: 5)
+							.stroke(Color("SettingsFieldBorder"), lineWidth: 1)
+					)
+				
+				VStack {
+					TextField("Secret Key", text: $secretKey, axis: .vertical)
+						.textFieldStyle(PlainTextFieldStyle())
+						.padding(7)
+					
+					Spacer()
 				}
+				.frame(height: 70)
+			}
+			.padding(.vertical, 5)
+			
+			HStack {
+				Spacer()
+				Button(action: {
+					print("Secret: \(secretKey)")
+					if !MBKeychain.shared.save(key: "Strata: Secret", value: secretKey) {
+						print("Error saving secret key")
+					}
+				}) {
+					Text("Save").frame(minWidth: 50)
+				}
+				.disabled((secretKey == oldSecretKey) || (secretKey.isEmpty))
 			}
 		}
 		.frame(width: 400)
 		.padding()
 		.onAppear() {
 			if let secret_key = MBKeychain.shared.get(key: "Strata: Secret") {
+				oldSecretKey = secret_key
 				secretKey = secret_key
 			}
 		}
